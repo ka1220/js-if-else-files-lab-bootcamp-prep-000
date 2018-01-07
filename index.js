45ea41211aa3a5a660e21d9ce95155d51ec8b6c3
Kaleab Admassu <kaladmassu@gmail.com>
2018-01-07 01:09:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var name = "Joe"
+var name = "Susan"
 var height = "74"
 
 if (name === "Susan")
 {
   console.log("The name is correct")
 }
-else 
+else
 {
     console.log("Expected the name to be Susan")
 }
@@ -13,4 +13,3 @@ else
 // Don't worry about this
 module.exports = { name, height
 }
-
